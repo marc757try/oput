b359b4cc46fe6d29c31b87ca0959dcb88a7be16b
marcd757@gmail.com <marcd757@gmail.com>
2019-05-24 20:19:10 +0300

    travis  +  eslint

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-"use strict"
-var http = require('http');
-var url = require('url');
+'use strict'
+var http = require('http')
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  res.end(txt);
-}).listen(8080);
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write('Hello World!')
+  res.end()
+}).listen(8080)
