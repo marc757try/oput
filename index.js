83a23c21e32c48dffa5736301dabbf5bfa1d5698
marcd757@gmail.com <marcd757@gmail.com>
2019-05-25 15:58:48 +0300

    osmosis

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 'use strict'
-var http = require('http')
+var TelegramBot = require('node-telegram-bot-api');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.write('Hello World!')
-  res.end()
-}).listen(8080)
+
+var token = '852827328:AAFOqo07RxcZJkItZj8PxwFi2tCrZ7k6Rzs';
+
+var bot = new TelegramBot(token, {polling: true});
+var notes=[]
+const osmosis=require('osmosis')
+osmosis
+   .get('https://privatbank.ua')
+   .find('.resourcescontent ul.app-card-grid')
+   .follow('li a[href]')
+   .find('.resourcescontent')
+   .set({
+       'appname': '.app-header__details h1',
+       'email': '#AppInfo table tbody tr:nth-child(2) td > a'
+    })
+   .log(console.log)   
+   .data(console.log)
+
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+  var userId = msg.from.id;
+  var text = match[1];
+  var time = match[2];
+
+  notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+  bot.sendMessage(userId, 'Отлично! :)');
+});
+setInterval(function(){
+  for (var i = 0; i < notes.length; i++){
+      var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+          if ( notes[i]['time'] == curDate ) {
+              bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+              notes.splice(i,1);
+          }
+      }
+},1000);
+
