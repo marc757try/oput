e05dcb2cee6edd64014e10b6e0e59b3a69e90e25
marcd757@gmail.com <marcd757@gmail.com>
2019-06-11 09:32:06 +0300

    jsdom code query not ok

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-const express = require('express')
-const axios = require('axios')
-//const { JSDOM } = require('jsdom')
-const request = require("request"),
- const   cheerio = require("cheerio"),
-const bodyParser = require('body-parser')
-const https=require('https')
+const express = require('express');
+const axios = require('axios');
+const { JSDOM } = require('jsdom');
+const bodyParser = require('body-parser');
 
-const token = process.env.CURS_BOT
-const appUrl = process.env.APP_URL
+const token = process.env.CURS_BOT;
+const appUrl = process.env.APP_URL;
 
-const setWebhook = url => axios.get(`https://api.telegram.org/bot${token}/setWebhook?url=${url}`)
-const sendMessage = (chatId, text) => axios.get(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`)
-const parseCurs = ()=>{
-  url = "https://privatbank.ua/"
+const setWebhook = url => axios.get(`https://api.telegram.org/bot${token}/setWebhook?url=${url}`);
+const sendMessage = (chatId, text) => axios.get(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`);
+const parseCurs = async (date) => {
+  const {
+    window: { document },
+  } = await JSDOM.fromURL('https://privatbank.ua/', {
+    resources: 'usable',
+    runScripts: 'dangerously',
+  });
+  const docp = Array.from(document.querySelectorAll('.main'));
+  const doc = docp.filter(el => el.querySelector('.content_conteiner_index');
+  return doc ? doc.querySelector('.').textContent : 'no info';
+};
 
-  request(url, function (error, response, body) {
-      if (!error) {
-          const $ = cheerio.load(body),
-              curs = $("#USD_buy").text()
-             // b = $("#USD_sell").text()
-                  }
-      else { return }
-  }
-}
-const app = express()
-//app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 app.post('/telegram', (req, res) => {
   const {
     text,
     chat: { id },
-  } = req.body.message
+  } = req.body.message;
   parseCurs(text).then(
     curs => sendMessage(id, curs),
     () => sendMessage(id, 'error'),
-  )
-  res.send()
-})
+  );
+  res.send();
+});
 app.get('*', (_req, res) => {
-  res.send(' Express.js')
-})
+  res.send('Express.js');
+});
 app.listen(process.env.PORT || 3000, () => {
-  setWebhook(`${appUrl}/telegram`)
-})
+  setWebhook(`${appUrl}/telegram`);
+});
